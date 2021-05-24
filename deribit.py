985456c7bf146fa90eaf56c1de1e2791969ba198
sebapi747 <sebapi747@github.com>
2021-05-25 00:43:39 +0800

    send alerts

--- deribit.py
+++ deribit.py
@@ -1,8 +1,28 @@
 import requests
 import csv
 import os
+import datetime as dt
 import config
 dirname = config.dirname
+smskey = config.smskey
+smsphone = condif.smsphone
+
+def sendSMS(msg):
+    url = 'https://rest.messagebird.com/messages'
+    headers = {'Authorization': 'AccessKey %s' % smskey}
+    myobj = {'recipients': smsphone, 'originator': smsphone, 'body':msg}
+    x = requests.post(url, data = myobj, headers=headers)
+    print(x.text)
+
+def sendMail(msg):
+    requests.post(
+        "https://api.mailgun.net/v3/mg.markowitzoptimizer.pro/messages",
+        auth=("api", config.MAILGUN_APIKEY),
+        data={"from": "admin <admin@mg.markowitzoptimizer.pro>",
+              "to": "admin@mg.markowitzoptimizer.pro",
+              "subject": msg,
+              "text": msg})
+
 tickers = ["BTC", "ETH"]
 suffix = ["-25JUN21", "-PERPETUAL", "-24SEP21", "-31DEC21", "-25MAR22", "-28MAY21"]
 for t in tickers:
@@ -21,4 +41,11 @@ for t in tickers:
             if fileexists == False:
                 w.writerow(dic.keys())
             w.writerow(dic.values())
+        if s!="-PERPETUAL":
+            continue
+        relSpd = dic['best_bid_price']/dic['estimated_delivery_price']
+        if relSpd<0.97:
+            msg = ("ALERT-%s: %s spd=%.2f%%" % (str(dt.datetime.utcnow()), t,relSpd))
+            sendSMS(msg)
+            sendMail(msg)
 
