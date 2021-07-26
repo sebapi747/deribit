d51c52ecd08b36651c1eda4d2c2b735dbfcd096e
sebapi747 <sebapi747@github.com>
2021-07-26 16:09:33 +0800

    format graph

--- update_daily_btc_csv.py
+++ update_daily_btc_csv.py
@@ -30,6 +30,21 @@ def but_atm_premium(F, T, r, sigma):
     put  =  call + (K-F) * df
     return call+put
 
+def bs_premium(F, K, T, r, sigma):
+    df = np.exp(-r * T) 
+    if T<1e-8:
+        call = max(K-F, 0.) * df
+        vega = 0
+    else:
+        sqt = np.sqrt(T)
+        d1 = (np.log(F / K) + (0.5 * sigma ** 2) * T) / (sigma * sqt)
+        d2 = d1 - sigma * sqt
+        df = np.exp(-r * T) 
+        call = (F * si.norm.cdf(d1, 0.0, 1.0) - K * si.norm.cdf(d2, 0.0, 1.0)) * df
+        vega = F * si.norm.pdf(d1, 0.0, 1.0) * sqt * df
+    put  =  call + (K-F) * df
+    return call, put, vega
+
 def get_csv(filename):
     df = pd.read_csv(filename)
     df = df.dropna() #[-300:]
@@ -58,20 +73,41 @@ def run_backtest(df):
     bars = [1, 2, 7, 14, 30, 91, 182]
     sigmas = [0.6, 0.7, 0.8, 0.9, 1., 1.1]
     res = {}
+    rescall = {}
+    resput = {}
     for b in bars:
         print("bar=%d" % b)
         res[b] = {}
+        rescall[b] = {}
+        resput[b] = {}
         for sigma in sigmas:
             t = b/365.
-            but = -but_atm_premium(1, t, 0 , sigma)
-            pnl = np.abs(df['Close'].diff(b)/df['Close'].shift(b)).fillna(0)
-            res[b][sigma] = pnl + but
+            c,p,v = bs_premium(1,1,t,0,sigma)
+            pnlc = (df['Close'].diff(b)/df['Close'].shift(b)).fillna(0)
+            pnlc[pnlc<0] = 0
+            rescall[b][sigma] = pnlc - c
+            pnlp = (df['Close'].diff(b)/df['Close'].shift(b)).fillna(0)
+            pnlp[pnlp>0] = 0
+            resput[b][sigma] = -pnlp - p
+            res[b][sigma] = pnlc-pnlp -p-c
     for b in bars:
         for sigma in sigmas:
-            plt.plot(df['Date'], np.cumsum(res[b][sigma]), label="vol=%f%%" % (sigma*100))
+            plt.plot(df['Date'], np.cumsum(res[b][sigma]), label="vol=%.f%%" % (sigma*100))
         plt.legend()
-        plt.title("cum pnl for %d days buterfly" % b)
+        plt.title("cum pnl for %d days butterfly" % b)
         plt.savefig("%sbuterfly-pnl-%dbar.png" % (outdir, b))
+        plt.close()
+        for sigma in sigmas:
+            plt.plot(df['Date'], np.cumsum(resput[b][sigma]), label="vol=%.f%%" % (sigma*100))
+        plt.legend()
+        plt.title("cum pnl for %d days put" % b)
+        plt.savefig("%sput-pnl-%dbar.png" % (outdir, b))
+        plt.close()
+        for sigma in sigmas:
+            plt.plot(df['Date'], np.cumsum(rescall[b][sigma]), label="vol=.%f%%" % (sigma*100))
+        plt.legend()
+        plt.title("cum pnl for %d days call" % b)
+        plt.savefig("%scall-pnl-%dbar.png" % (outdir, b))
         plt.close()
     return res
 
