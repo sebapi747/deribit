f5ea797856acbadea444eec901f590e50b2837af
seb <seb@hermes.com>
2023-11-22 19:41:27 +0800

    test imm dates

--- deribit.py
+++ deribit.py
@@ -28,20 +28,44 @@ def sendTelegram(text):
     resp = requests.post('https://api.telegram.org/bot{}/sendMessage'.format(config.telegramtoken), params)
     resp.raise_for_status()
 
-tickers = ["BTC", "ETH"]
-suffix = ["-PERPETUAL"]
-dtnow = dt.datetime.now()
-for futm in [3,6,9,12]:
+def firstnextmonth(date):
+    mt = dtnow.month+1
     yr = dtnow.year
-    mt = 3*(dtnow.month//3)+futm+1
-    if mt>12:
-        yr = yr+1
-    futdt = dt.datetime(yr,1+(mt-1)%12,1)-dt.timedelta(days=1)
-    for i in range(7):
-        if futdt.weekday()==4:
-            break
-        futdt = futdt-dt.timedelta(days=1)
-    suffix.append(futdt.strftime("-%d%b%y").upper())
+    yr = yr+mt//12
+    return dt.datetime(yr,1+(mt-1)%12,1)
+
+def monthend(date):
+    return firstnextmonth(date)-dt.timedelta(days=1)
+
+def isImmMonth(date):
+    return date.month%3==0 
+
+def daysToMonthEnd(date):
+    return int((monthend(date)-date).total_seconds()/3600/24)
+
+def isAfterImm(date):
+    return isImmMonth(date) and (date.weekday()+3)%7+daysToMonthEnd(date)<7
+
+def list_suffix(dtnow):
+    futdates = []
+    for futm in [3,6,9,12]:
+        yr = dtnow.year
+        if isAfterImm(dtnow):
+            mt = dtnow.month+futm+1
+        else:
+            mt = 1+3*((dtnow.month-1)//3)+futm
+        yr = yr+mt//12
+        mt = 1+(mt-1)%12
+        futdt = dt.datetime(yr,mt,1)-dt.timedelta(days=1)
+        daytofriday = (futdt.weekday()+3)%7
+        futdt = futdt-dt.timedelta(days=daytofriday)
+        if futdt<=dtnow:
+            raise Exception("problem with dt fut=%s now=%s" % (futdt,dtnow))
+        futdates.append(futdt)
+    return ["-PERPETUAL"] + [d.strftime("-%d%b%y").upper() for d in sorted(futdates)]
+
+tickers = ["BTC", "ETH"]
+suffix = list_suffix(dt.datetime.now())
 
 for t in tickers:
     for s in suffix:
