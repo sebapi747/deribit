23737a056058f36ce64cf2765237291cb55af073
seb <sebastien.hitier@gmail.com>
2025-03-10 17:07:36 +0800

    use agent list

--- immfutyahoo.py
+++ immfutyahoo.py
@@ -222,20 +222,9 @@ def get_futjson_data(ticker):
     print("INFO: %s in %.2fsec" % (url,sleeptime))
     time.sleep(sleeptime)
     os.system("rm -f %s" % filename)
-    cmd = f"""curl --silent '{url}' \
-    -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-    -H 'accept-language: en-US,en;q=0.9' \
-    -H 'cache-control: max-age=0' \
-    -H 'priority: u=0, i' \
-    -H 'sec-ch-ua: "Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"' \
-    -H 'sec-ch-ua-mobile: ?0' \
-    -H 'sec-ch-ua-platform: "Linux"' \
-    -H 'sec-fetch-dest: document' \
-    -H 'sec-fetch-mode: navigate' \
-    -H 'sec-fetch-site: none' \
-    -H 'sec-fetch-user: ?1' \
-    -H 'upgrade-insecure-requests: 1' \
-    -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' > '{filename}' """
+    ua = random.choice(pd.read_csv("agent.csv",sep="|").agent)
+    cmd = f"curl --silent '{url}' -H 'user-agent: {ua}' > '{filename}'"
+    os.system(cmd)
     try:
         if not os.path.exists(filename):
             raise Exception("ERR: could not fetch %s" % url)
